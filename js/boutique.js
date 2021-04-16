465d549d2e3fd154e8230aabe12ae7a0fa1b79ad
Tombal <nicolas.tombal01@gmail.com>
2021-04-16 15:46:20 +0200

    JS & Research bar

--- js/boutique.js
+++ js/boutique.js
@@ -1,14 +1,42 @@
 class Boutique {
-    constructor() {
+    constructor() 
+    {
         this.produits = [];
         this.categorie = "cameras";
     }
+
     recupereProduits() {
-        fetch(`http://localhost:3000/api/${this.categorie}`).then(function(responce){
-            responce.json().then(function(produits){ 
-            });
+        fetch(`http://localhost:3000/api/${this.categorie}`)
+        .then(responce => responce.json())
+        .then(produits => { 
+            this.produits = produits;
+            this.afficheProduits();
         });
     }
+
+    afficheProduits() {
+        let i;
+        for(i = 0; i < this.produits.length; i++) {
+            document.getElementById("listeproduit")
+            .insertAdjacentHTML("beforeend", `
+            <div class="col col-lg-3">
+                <div class="card shadow border-info mb-5">
+                    <img src="${this.produits[i].imageUrl}" alt="">
+                    <div class="card-body">
+                        <h3 class="card-title">${this.produits[i].name}</h3>
+                        <p class="card-text lead mb-2">Prix : ${this.produits[i].price}€</p>
+                        <p class="card-text">${this.produits[i].description}</p>
+                        <a href="produit.html" class="btn btn-info"><i class="fas fa-info mr-2"></i>Voir ce produit</a>          
+                    </div>
+                </div>
+            </div>
+            `);
+        }
+    }
 }
+
+
+
+
 
 let boutique = new Boutique();
