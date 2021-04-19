4b59082cd61b51d72388453a850e87c3b438ab24
Tombal <nicolas.tombal01@gmail.com>
2021-04-19 17:26:56 +0200

    color title & logo

--- js/boutique.js
+++ js/boutique.js
@@ -5,15 +5,18 @@ class Boutique {
         this.categorie = "cameras";
     }
 
+    // Requête vers l'API //
     recupereProduits() {
         fetch(`http://localhost:3000/api/${this.categorie}`)
         .then(responce => responce.json())
         .then(produits => { 
             this.produits = produits;
             this.afficheProduits();
+            console.table(this.produits);
         });
     }
 
+    // Affiche les produits dans l'ordre dans index.html //
     afficheProduits() {
         let i;
         for(i = 0; i < this.produits.length; i++) {
@@ -21,7 +24,7 @@ class Boutique {
             .insertAdjacentHTML("beforeend", `
             <div class="col col-lg-3">
                 <div class="card shadow border-info mb-5">
-                    <img src="${this.produits[i].imageUrl}" alt="">
+                    <img src="${this.produits[i].imageUrl}" alt="Produit appareil photo">
                     <div class="card-body">
                         <h3 class="card-title">${this.produits[i].name}</h3>
                         <p class="card-text lead mb-2">Prix : ${this.produits[i].price}€</p>
@@ -32,6 +35,11 @@ class Boutique {
             </div>
             `);
         }
+    }
+
+    // Affiche le produit sollicité dans produit.html // 
+    detailProduits() {
+
     }
 }
 
